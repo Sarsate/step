f390ea7b6e69c3e02417cdff7fd22539a9b87c0f
Chris <christopher.desir01@gmail.com>
2020-06-01 14:45:34 -0400

    Gets rid of unnecessary comments

--- portfolio/src/main/webapp/script-home.js
+++ portfolio/src/main/webapp/script-home.js
@@ -48,11 +48,7 @@ function main() {
         function adjustForwardOne() {
           slideShowGallery.adjustSlideManual(ADJUST_FORWARD); 
     }
-
-    // fetch('/data').then((response) => response.text()).then((introHeading) => {
-    //     document.getElementById('intro-heading').innerText = introHeading;
-    //     document.getElementById('heading-button').style.display = 'none';
-    // });
+    
     fetch('/data').then(response => response.json()).then((comments) => {
       const /** ?HTMLCollection */commentContainer =
           document.getElementById('comments-container');
